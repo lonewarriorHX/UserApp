2a8dab282691897174ae49903a7dd627bf60016c
Hanvey <hanveyxavero888@gmail.com>
2020-11-22 15:57:34 +0700

    belum jadi

--- app/src/main/java/com/example/finalapplication/HomeFragment.kt
+++ app/src/main/java/com/example/finalapplication/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.example.finalapplication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
