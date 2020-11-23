950bea6dee8424ccf1b987ab1bdec4984d853cdb
Hanvey <hanveyxavero888@gmail.com>
2020-11-23 22:17:43 +0700

    screen

---
+++ app/src/main/java/com/example/finalapplication/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,38 @@
+package com.example.finalapplication.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.finalapplication.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ViewPagerFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ViewPagerFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
+
+        val fragmentList = arrayListOf<Fragment>(
+
+        )
+
+        return view
+    }
+}
