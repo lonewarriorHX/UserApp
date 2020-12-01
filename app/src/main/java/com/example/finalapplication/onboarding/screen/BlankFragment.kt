ce157458149df2558722df6a06fd26dbdb179105
Hanvey <hanveyxavero888@gmail.com>
2020-12-01 13:15:21 +0700

    start button

--- app/src/main/java/com/example/finalapplication/onboarding/screen/BlankFragment.kt
+++ app/src/main/java/com/example/finalapplication/onboarding/screen/BlankFragment.kt
@@ -1,15 +1,20 @@
 package com.example.finalapplication.onboarding.screen
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.example.finalapplication.DashboardActivity
 import com.example.finalapplication.R
+import com.example.finalapplication.onboarding.FormPage
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +31,8 @@ class BlankFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        val view =inflater.inflate(R.layout.fragment_blank, container, false)
+        val btn5 = view.findViewById<View>(R.id.btn5) as TextView
         Handler().postDelayed({
             if(onBoardingFinished()){
                 findNavController().navigate(R.id.action_blankFragment_to_mainActivity)
@@ -36,8 +42,15 @@ class BlankFragment : Fragment() {
             }
         }, 3000)
 
+        btn5.setOnClickListener{
+            requireActivity().run{
+                startActivity(Intent(this, DashboardActivity::class.java))
+                finish()
+            }
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        return view
     }
 
     private fun onBoardingFinished(): Boolean{
